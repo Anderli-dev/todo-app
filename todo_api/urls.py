4ed7bd2988557e9916878c17f929b5f4bc89f715
Ivan Suprun <ivan.suprun2015@gmail.com>
2022-01-24 20:42:49 +0200

    build(api): add Rest api to project
    Create serializer.py. add 2 view in view.py add 2 url in urls.py

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import UserView, TaskView
 
 urlpatterns = [
-    path('', main),
+    path('user/', UserView.as_view()),
+    path('task/', TaskView.as_view()),
 ]
--- todo_api/views.py
+++ todo_api/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+
+from .models import CustomUser, Task
+from .serializer import UserSerializer, TaskSerializer
 
 
-def main(request):
-    return HttpResponse('<h1>Hello world<h1>')
+class UserView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+
+class TaskView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
