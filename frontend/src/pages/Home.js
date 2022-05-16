57db8eb632b90dfae6136de7b7eb8a7adb710ec3
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2022-05-16 18:16:40 +0300

    refactor(): add lazy load

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,194 +1,56 @@
-import React, {useEffect, useState} from "react";
-import {Button, Table} from "react-bootstrap";
-import {Scrollbars} from 'react-custom-scrollbars';
-import axios from "axios";
+import React, {lazy, Suspense, useState} from "react";
 import Cookies from "js-cookie";
-import {UpdateStatus} from "../actions/UpdateStatus"
-import {DeleteTask} from "../actions/DeleteTask";
-import {CreateWhiteIco} from "../actions/CreateWhiteIco";
-import {MdDeleteForever, MdEdit, MdOutlineCheckBox, MdOutlineCheckBoxOutlineBlank} from "react-icons/md";
+import {Spinner} from "react-bootstrap";
+import axios from "axios";
+
+const TodoList = lazy(()=>import('../components/TodoList'))
 
 export function Home(){
-    let [tasks, setTasks] = useState([]);
     const isAuth = Cookies.get("logged_in")
+    const [isLoad] = useState(true)
+    const [isData, setIsData] = useState(true);
 
     function getTasks(){
-        const headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        };
-        try {
-            axios.get(`${process.env.REACT_APP_API_URL}/api/tasks/`, {
-                headers: headers,})
-                .then(response => {setTasks(response.data)})
-                .catch(error => console.log(error))
-        } catch (err) {
-            console.log(err)
-        }
-    }
-
-    function delTask(id){
-        DeleteTask(id)
-
-        let index = 0;
-        for(index; index<tasks.length; index++) {
-            if (tasks[index].id === id) {
-                break;
-            }
-        }
-        const newList=[];
-        for(let i=0; i < tasks.length; i++){
-            if(i !== index) {
-                newList.push(tasks[i])
-            }
-        }
-        setTasks(newList);
-
-        getTasks()
-    }
-
-    function doneTask(id){
-        const body = {
-            task_status:"done"
-        }
-        
-        UpdateStatus(body, id)
-
-        let index = 0;
-        for(index; index<tasks.length; index++) {
-            if (tasks[index].id === id) {
-                break;
+            const headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            };
+            try {
+                axios.get(`${process.env.REACT_APP_API_URL}/api/tasks/`, {
+                    headers: headers,})
+                    .then(response => {if (!response.data.length) {setIsData(false) }
+                                       else {setIsData(true);}
+                    })
+                    .catch(error => console.log(error))
+            } catch (err) {
+                console.log(err)
             }
         }
 
-        const newList=[];
-        for(let i=0; i < tasks.length; i++){
-            if(i !== index) {
-                newList.push(tasks[i])
-            }
-            else {
-                tasks[i].is_done = true
-                newList.push(tasks[i])
-            }
-        }
-
-        setTasks(newList);
-    }
-
-    function unDoneTask(id){
-        const body = {
-            task_status:"un_done"
-        }
-
-        UpdateStatus(body, id)
-
-        let index = 0;
-        for(index; index<tasks.length; index++) {
-            if (tasks[index].id === id) {
-                break;
-            }
-        }
-
-        const newList=[];
-        for(let i=0; i < tasks.length; i++){
-            if(i !== index) {
-                newList.push(tasks[i])
-            }
-            else {
-                tasks[i].is_done = false
-                newList.push(tasks[i])
-            }
-        }
-
-        setTasks(newList);
-
-    }
-
-    useEffect(() => {
-        getTasks();
-    }, []);
-
-    const notHaveTodoMsg = () => {
+    function todoList(){
         return(
             <>
-                <div className="d-flex flex-column" style={{height: "69vh"}}>
-                    <div className="">
-                        <h2 className="text-muted mt-5 text-center">You dont have todo-s yet...</h2>
-                        <h1 className="text-muted text-center ">:(</h1>
-                    </div>
-                    <div className="text-center mt-auto mt-5"><a href={"/task/create"} >Clik hear to create one</a></div>
-                </div>
+                {isLoad && (
+                    <Suspense fallback={
+                        <div style={{marginTop:"30%"}} className="container d-flex justify-content-center">
+                            <Spinner className="" animation="border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </Spinner>
+                        </div>
+                    }>
+
+                        <TodoList isData={isData}/>
+                    </Suspense>
+                )}
+
             </>
         )
     }
 
-    const todoList = () => {
-      return(
-          <>
-              <div className="card" style={{backgroundColor: "#7a8794"}}>
-                  <div className="card-body" data-mdb-perfect-scrollbar="true">
-                      <Table striped bordered hover variant="dark" className="m-0" >
-                          <thead>
-                          <tr>
-                              <th className="border-bottom">Task</th>
-                              <th className="text-center w-25 border-bottom">Actions</th>
-                          </tr>
-                          </thead>
-                      </Table>
-                      <Scrollbars style={{ width: "100%", height: "47vh"}}>
-                          <Table striped bordered hover variant="dark">
-                              <tbody>
-                              {tasks.map(item => (
-                                  <tr key = { item.id }>
-                                      <td><a href={`/task/${item.id}`}
-                                             className={item.is_done?"d-flex text-decoration-line-through text-white"
-                                                 :"d-flex text-decoration-none text-white"}>{item.title}<div className="ms-2">{CreateWhiteIco(<MdEdit />)}</div></a>
-                                      </td>
-                                      <td className="w-25 text-center">
-                                          {item.is_done
-                                              ?<Button className="me-3 pb-2" onClick={()=>unDoneTask(item.id)}>
-                                                  {CreateWhiteIco(<MdOutlineCheckBox/>)}
-                                               </Button>
-                                              :<Button className="me-3 pb-2" onClick={()=>doneTask(item.id)}>
-                                                      {CreateWhiteIco(<MdOutlineCheckBoxOutlineBlank/>)}
-                                               </Button>
-                                          }
-                                          <Button className="pb-2 btn-danger" onClick={()=>delTask(item.id)}>{CreateWhiteIco(<MdDeleteForever />)}</Button>
-                                      </td>
-                                  </tr>
-                              ))
-                              }
-                              </tbody>
-                          </Table>
-                      </Scrollbars>
-                  </div>
-                  <div className="card-footer text-end p-3">
-                      <Button href="task/create" className="btn btn-secondary">Add Task</Button>
-                  </div>
-              </div>
-          </>
-      )
-    }
-
     const authContent = () => {
-      return(
-          <>
-              <div className="h1 my-2 pb-3 text-center">
-                  <i className="fas fa-check-square me-1"></i>
-                  <u>My Todo-s</u>
-              </div>
-              {tasks
-                  ?   <>
-                      {tasks.length === 0 && tasks
-                          ? notHaveTodoMsg()
-                          : todoList()
-                      }
-                      </>
-                  :
-                  <></>
-              }
-            </>
-      )
+        return(
+            todoList()
+        )
     }
 
     const guestContent = ()=>{
@@ -207,7 +69,6 @@ export function Home(){
         <div>
             <div className="dark">
                 {isAuth
-                    // TODO add lazy
                     ? authContent()
                     : guestContent()
                 }
