11ea62cb6aa2ce98a633ea263a11feb5f7c0e436
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2022-05-19 07:55:52 +0300

    refactor(): fix code

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import static
+
+from todo_app import settings
 
 urlpatterns = [
     path('', include('frontend.urls'), name="home"),
     path('admin/', admin.site.urls),
     path('api/', include('todo_api.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
